499f7e304c37f501c963b4c50fba93d8d1c4ce6c
Ильназ Газимов <ilnaz.gazimov@mail.ru>
2021-12-20 10:58:03 +0500

    Добавьте файлы проекта.

---
+++ AdminMobile/AdminMobile/About.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AdminMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class About : ContentPage
+    {
+        public About()
+        {
+            InitializeComponent();
+            using(var http = new HttpClient())
+            {
+                var res = http.GetAsync($"http://drankmin-001-site1.htempurl.com/api/lastdance/allIzm");
+                var response = res.Result.Content.ReadAsStringAsync();
+                grip.Text = response.Result;
+            }
+        }
+    }
+}
---
+++ AdminMobile/AdminMobile/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AdminMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new About());
+        }
+
+        private async void ff(object sender, EventArgs e)
+        {
+            using (var http = new HttpClient())
+            {
+                var res = await http.GetAsync($"http://drankmin-001-site1.htempurl.com/api/lastdance/addFormat/{addFormat.Text}");
+                res.EnsureSuccessStatusCode();
+                await DisplayAlert("Отлично", "Все хорошо", "Ok");
+            }
+        }
+
+        private async void ss(object sender, EventArgs e)
+        {
+            using (var http = new HttpClient())
+            {
+                var res = await http.GetAsync($"http://drankmin-001-site1.htempurl.com/api/lastdance/DeleteIzm/{deleteIzm.Text}");
+                res.EnsureSuccessStatusCode();
+                await DisplayAlert("Отлично", "Все хорошо", "Ok");
+            }
+        }
+    }
+}
